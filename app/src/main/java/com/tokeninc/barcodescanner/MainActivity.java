0d175823592a3eaecc55e39f2615c0fce9472683
Ege Ekici <ege.ekici@tokeninc.com>
2020-01-21 10:42:22 +0300

    barcode scanner implemented

---
+++ app/src/main/java/com/tokeninc/barcodescanner/MainActivity.java
@@ -0,0 +1,43 @@
+package com.tokeninc.barcodescanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent =new Intent(MainActivity.this,ContinuousCaptureActivity.class);
+        startActivityForResult(intent,1);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Log.d("results", data.getStringArrayListExtra("result").toString());
+        }
+
+    }
+
+
+
+}
